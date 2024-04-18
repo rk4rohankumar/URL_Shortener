fb10efd8ec3ea31bbd3f31d283fe1cf5f2d2bc89
Rohan Kumar <96616262+rk4rohankumar@users.noreply.github.com>
2024-04-19 00:44:44 +0530

    enviroment variable resolved

--- server/app.js
+++ server/app.js
@@ -2,16 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const apiRoutes = require('./routes/api');
-const config = require('./config');
+require('dotenv').config();
+
 
 const app = express();
-
+const mongoURI = process.env.mongoURI;
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
